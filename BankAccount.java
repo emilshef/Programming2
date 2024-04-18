177ac833358b1286a979e1861968c8f393dcf963
emilshef <160155829+emilshef@users.noreply.github.com>
2024-04-18 10:32:01 -0500

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,64 @@
+
+public class BankAccount {
+
+	private String firstName;
+	private String lastName;
+	private int accountID;
+	private double balance;
+	
+	//constructor
+	public BankAccount () {
+		this.firstName = "N/a";
+		this.lastName = "N/a";
+		this.accountID = 0;
+		this.balance = 0;
+	}
+	
+	//deposit method
+	public void deposit (double dollarAmount) {
+		balance += dollarAmount;
+	}
+	
+	//withdrawal method 
+	public void withdrawal (double dollarAmount) {
+		balance -= dollarAmount;
+	}
+		
+	public void setFirstName (String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public void setLastName (String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public void setAccountID (int accountID) {
+		this.accountID = accountID;
+	}
+	
+	public void getFirstName() {
+		System.out.println("First name: " + firstName);
+	}
+	
+	public void getLastName() {
+		System.out.println("Last name: " + lastName);
+	}
+	
+	public void getAccountID() {
+		System.out.println("Account ID: " + accountID);
+	}
+
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void accountSummary() {
+		System.out.println("Here is a summary for this account: ");
+		System.out.println("First name: " + firstName);
+		System.out.println("Last name: " + lastName);
+		System.out.println("Account ID: " + accountID);
+		System.out.println("Balance " + balance);
+			
+	}
+}
